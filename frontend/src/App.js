a26f84071efab881fc1d4760cef937e5ab7bba73
Jwhitehead-drft <67030406+Jwhitehead-drft@users.noreply.github.com>
2021-03-18 15:38:25 -0400

    react init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  // returned output must be in single element. aditional div / etc will not work
+  return (
+    <>
+      <h1>Welcome to Sparkles and Swirls!</h1>
+    </>
+  )
+}
+
+export default App;
